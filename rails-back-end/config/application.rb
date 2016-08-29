b44118a6428f5ac851516520afd6aa76cd397174
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-08-29 16:00:25 -0400

    get rails configured so device will play nicely with react

--- rails-back-end/config/application.rb
+++ rails-back-end/config/application.rb
@@ -16,6 +16,10 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.to_prepare do
+  DeviseController.respond_to :html, :json
+end
+
 module LesFilmFestDewt
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
--- rails-back-end/config/routes.rb
+++ rails-back-end/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   root to: "home#index"
+
+  scope :auth do
+    get 'is_signed_in', to: 'auth#is_signed_in?'
+  end
+
 end
