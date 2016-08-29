27408d5241e7f6cf1dd39ec8ab426c9c0f4ca0d0
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-08-29 15:29:50 -0400

    get devise setup for user routing

--- rails-back-end/db/schema.rb
+++ rails-back-end/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160829154338) do
+ActiveRecord::Schema.define(version: 20160829185219) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body",       null: false
@@ -61,13 +61,23 @@ ActiveRecord::Schema.define(version: 20160829154338) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",           null: false
+    t.string   "username",                           null: false
     t.string   "full_name"
-    t.string   "email",              null: false
-    t.string   "encrypted_password", null: false
-    t.string   "type",               null: false
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.string   "email",                              null: false
+    t.string   "encrypted_password",                 null: false
+    t.string   "type",                               null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "votes", force: :cascade do |t|
@@ -77,7 +87,7 @@ ActiveRecord::Schema.define(version: 20160829154338) do
     t.datetime "created_at",                                                            null: false
     t.datetime "updated_at",                                                            null: false
     t.integer  "user_id"
-    t.integer  "#<ActiveRecord::ConnectionAdapters::TableDefinition:0x007fc49ac15690>"
+    t.integer  "#<ActiveRecord::ConnectionAdapters::TableDefinition:0x007f89efc61198>"
   end
 
 end
