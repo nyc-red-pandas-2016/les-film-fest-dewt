da918f575753aec293c17eac767e9a285bc12229
David Lin <dave.lin21@gmail.com>
2016-08-29 20:23:22 -0400

    add review and comment components

--- rails-back-end/config/routes.rb
+++ rails-back-end/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: "users/registrations"}
   get 'movies/:id', to: 'movie#show'
 
+  get 'reviews/:id', to: 'review#show'
+  get 'comments/:id', to: 'comment#show'
+
   root to: "home#index"
 
   scope :auth do
